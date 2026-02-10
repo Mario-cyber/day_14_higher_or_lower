ea4fa5a266d8b26b33d2789f002cf7ef1fc283f2
Mario_F <54963398+Mario-cyber@users.noreply.github.com>
2026-02-10 02:50:44 +0000

    data imported, funtionality layout done

---
+++ higher_or_lower.py
@@ -0,0 +1,11 @@
+from game_data import data 
+
+# compare game should 
+# 1) print the first contender in and f string such that 
+# Compare A : 'name' , a 'description' , from 'country'
+# Present the comparasion with a similar line 
+# compare the value of followers 
+
+#potentially make two fucntion, one to present the game and one two actuallly compare
+
+
