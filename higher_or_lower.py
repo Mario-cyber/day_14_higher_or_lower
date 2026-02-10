260456a8c953e653c3631cd32442237b2902a464
Mario_F <54963398+Mario-cyber@users.noreply.github.com>
2026-02-10 22:23:16 +0000

    introduction to the game, done

--- higher_or_lower.py
+++ higher_or_lower.py
@@ -9,10 +9,14 @@ import random
 
 #potentially make two fucntion, one to present the game and one two actuallly compare
 
+print("welcome to higher or lower")
 
-option_a = data[random.randint(0,len(data)-1)]['name']
-option_b  = data[random.randint(0,len(data)-1)]['name']
+option_a = data[random.randint(0,len(data)-1)]
+option_b  = data[random.randint(0,len(data)-1)]
 
-print(option_a)
-print(option_b)
+print(f"Choose between option A: {option_a['name']}, a {option_a['description']} from {option_a['country']}")
+print(f"Or option B : {option_b['name']}, a {option_b['description']} from {option_b['country']}")
+
+
+
 
