a5344b2f6f1b9640a1939a8ed639023f08ff6f51
Mario_F <54963398+Mario-cyber@users.noreply.github.com>
2026-02-10 02:42:02 +0000

    example data added

---
+++ game_data.py
@@ -0,0 +1,28 @@
+data = [
+    {
+        'name' : 'Naurto', 
+        'follower count' : 500,
+        'description' : 'anime about a ninja boy',
+        'country': 'Japan'
+    },
+    {
+        'name' : 'soccer', 
+        'follower count' : 1200,
+        'description' : 'worldwide sport',
+        'country': 'global'
+    },
+    {
+        'name' : 'Tony Bourdain', 
+        'follower count' : 700,
+        'description' : 'chef and tv host',
+        'country': 'United States'
+    },
+    {
+        'name' : 'Rober Caro', 
+        'follower count' : 200,
+        'description' : 'Author of the Power Broker',
+        'country': 'United States'
+    }
+]
+   
+    
