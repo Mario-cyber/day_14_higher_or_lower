7c10b2b9bc1d45f73cd56470aa59dba750d3d3d4
Mario_F <54963398+Mario-cyber@users.noreply.github.com>
2026-02-12 17:04:12 +0000

    index fixed, options presented, needs game logic

--- functions.py
+++ functions.py
@@ -5,6 +5,11 @@ index_list  = [0]
 index_a = 0
 index_b = 0 
 
+option_a = {}
+option_b = {}
+
+user_choice = ""
+
 def populate_index():
     new_index = 0
     while len(index_list) < len(data):
@@ -27,7 +32,24 @@ def select_index():
 
 
 def present_options():
+    global option_a, option_b
     option_a = data[index_a]
     option_b  = data[index_b]
-    print(option_a)
-    print(option_b) 
+    print(f"Choose between option A: {option_a['name']}, a {option_a['description']} from {option_a['country']}")
+    print(f"Or option B : {option_b['name']}, a {option_b['description']} from {option_b['country']}")
+
+
+def user_choice():
+    
+    followers_a =  option_a['follower_count']
+    followers_b =  option_b['follower_count']
+    user_choice = input("type A or B: ").capitalize()
+    if user_choice == 'A' :
+        user_guess = followers_a
+        print("outcome #1")
+        # remaining_choice = followers_b
+    elif user_choice == 'B' :
+        user_guess = followers_b
+        print("outcome #2")
+        # remaining_choice = followers_a
+    return user_guess
--- higher_or_lower.py
+++ higher_or_lower.py
@@ -22,31 +22,24 @@ fn.select_index()
 
 fn.present_options()
 
-
-# option_a = data[index_a]
-# option_b  = data[index_b]
-
-# print(option_a)
-# print(option_b)
-
-# print(f"Choose between option A: {option_a['name']}, a {option_a['description']} from {option_a['country']}")
-# print(f"Or option B : {option_b['name']}, a {option_b['description']} from {option_b['country']}")
+fn.user_choice()
 
 
 
 
-def user_choice():
+
+#def user_choice():
    
-    followers_a =  option_a['follower_count']
-    followers_b =  option_b['follower_count']
-    user_choice = input("type A or B: ").capitalize()
-    if user_choice == 'A' :
-        user_guess = followers_a
-        # remaining_choice = followers_b
-    elif user_choice == 'B' :
-        user_guess = followers_b
-        # remaining_choice = followers_a
-    return user_guess
+#     followers_a =  option_a['follower_count']
+#     followers_b =  option_b['follower_count']
+#     user_choice = input("type A or B: ").capitalize()
+#     if user_choice == 'A' :
+#         user_guess = followers_a
+#         # remaining_choice = followers_b
+#     elif user_choice == 'B' :
+#         user_guess = followers_b
+#         # remaining_choice = followers_a
+#     return user_guess
 
 
 
